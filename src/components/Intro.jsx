43da8d35a881a207da58a947627c4f4072885db6
martu <martuu.amengual@gmail.com>
2021-01-16 17:43:40 -0300

    a

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -2,63 +2,65 @@
 import React, { Component } from 'react'
 import '../styles/Intro.sass'
 import location from '../images/location.svg'
+import ModalImage from './ModalImage';
 
 export default class Intro extends Component {
 
-    componentDidMount() {
-        document.title = 'CV - Martina Amengual'
-    }
-
     state = {
-        name: 'MARTINA',
+        name: 'MARTIN',
         surname: 'AMENGUAL',
         location: 'Buenos aires, Argentina'
     };
 
     render() {
         return (
-            <div className="container pt-lg-4">
-                <div className="row justify-content-center align-items-center text-center">
-                    <div className="col-lg">
-                        <span className="mtu-intro-name">{this.state.name}</span>
-                    </div>
-                    <div className="col-lg-offset">
-                        <div className="mtu-intro-foto shadow"></div>
-                    </div>
-                    <div className="col-lg">
-                    <span className="mtu-intro-name">{this.state.surname}</span>
-                    </div>
-                </div>
-                <div className="row mt-4 justify-content-center align-items-center text-center">
-                    <div className="col-lg">
-                    </div>
-                    <div className="col-lg-offset">
-                        <a href="https://goo.gl/maps/9FcCa95YWViybffL7" target="_blank" className="mtu-intro-icon d-block">
-                            <img src={location} alt="Location"></img>
-                            <span>{this.state.location}</span>
-                        </a>
-                        <span className="d-block mt-2">Web Developer</span>
-                        <span className="d-block mt-2">(Current-Job) Not-Employed</span>
-                    </div>
-                    <div className="col-lg">
-                    </div>
-                </div>
-                <div className="row mt-4 justify-content-center align-items-center text-center">
-                    <div className="col-lg"></div>
-                    <div className="col-lg-offset" style={{ maxWidth: "70%" }}>
-                        <div className="mtu-intro-description">
-                            Hola, bienvenid@ me llamo martina tengo 22 años 
-                            y soy desarrolladora de sistemas informaticos. Aprendi a programar 
-                            a los 8 años en C++, programe calculadoras, sistemas de calculo matematico 
-                            sensillo, etc. En la secundaria me especialize en <strong>electronica</strong>, 
-                            ahi desarroye y programe algunos robots y obtuvimos el 4to puesto (ver imagen). 
-                            Actualmente me encuentro aprediendo nuevas tecnologias. Soy una persona 
-                            amable, calida y aplicada. Si quieres me puedes <a href="#"><strong>contactar</strong></a> 😊
+            <section>
+                <div className="container pt-lg-4">
+                    <div className="row justify-content-center align-items-center text-center">
+                        <div className="col-lg">
+                            <span className="mtu-intro-name">{this.state.name}</span>
+                        </div>
+                        <div className="col-lg-offset">
+                            <div className="mtu-intro-foto shadow"></div>
+                        </div>
+                        <div className="col-lg">
+                        <span className="mtu-intro-name">{this.state.surname}</span>
                         </div>
                     </div>
-                    <div className="col-lg"></div>
+                    <div className="row mt-4 justify-content-center align-items-center text-center">
+                        <div className="col-lg">
+                        </div>
+                        <div className="col-lg-offset">
+                            <a href="https://goo.gl/maps/9FcCa95YWViybffL7" target="_blank" className="mtu-intro-icon d-block">
+                                <img src={location} alt="Location"></img>
+                                <span>{this.state.location}</span>
+                            </a>
+                            <span className="d-block mt-2">Web Developer</span>
+                            <span className="d-block mt-2">(Current-Job) Not-Employed</span>
+                        </div>
+                        <div className="col-lg">
+                        </div>
+                    </div>
+                    <div className="row mt-4 justify-content-center align-items-center text-center">
+                        <div className="col-lg"></div>
+                        <div className="col-lg-offset" style={{ maxWidth: "70%" }}>
+                            <div className="mtu-intro-description">
+                                Hola, bienvenid@ me llamo martina tengo 22 años 
+                                y soy desarrolladora de sistemas informaticos. Aprendi a programar 
+                                a los 8 años en C++, programe calculadoras, sistemas de calculo matematico 
+                                sensillo, etc. En la secundaria me especialize en <strong>electronica</strong>,
+                                ahi desarroye y programe algunos robots y obtuvimos el 4to puesto <ModalImage 
+                                id="asd1" id-btn="myImg1" text="(ver imagen)" 
+                                src="https://media.sproutsocial.com/uploads/2017/02/10x-featured-social-media-image-size.png" 
+                                alt="Example text2"></ModalImage>. 
+                                Actualmente me encuentro aprediendo nuevas tecnologias. Soy una persona 
+                                amable, calida y aplicada. Si quieres me puedes <a href="#"><strong>contactar</strong></a> 😊
+                            </div>
+                        </div>
+                        <div className="col-lg"></div>
+                    </div>
                 </div>
-            </div>
+            </section>
         );
     }
 }
---
+++ src/components/ModalImage.jsx
@@ -0,0 +1,38 @@
+import { Component } from "react";
+import '../styles/ModalImage.sass'
+
+
+export default class ModalImage extends Component {
+
+    handleClick = (data) => {
+        //let modal = event.target.href;
+        let a = data.target.parentElement;
+        let modal = a.parentElement;
+
+        
+        let modal_content = modal.querySelector('.mtu-modal-content');
+        modal_content.style.display = "block";
+
+        var span = modal_content.querySelector('.close');
+        span.onclick = function () {
+            modal_content.style.display = "none";
+        }
+    }
+
+    render() {
+        return (
+            <section className="mtu-modal-section">
+                <div id={'modal-image-' + this.props.id} className="mtu-modal">
+                    <a id={this.props['id-btn']} href="#" onClick={this.handleClick}>
+                        <span>{this.props.text}</span>
+                    </a>
+                    <div className="mtu-modal-content">
+                        <span className="close">&times;</span>
+                        <img className="modal-image" src={this.props.src} id="img01" />
+                        <div id="caption">{this.props.alt}</div>
+                    </div>
+                </div>
+            </section>
+        );
+    }
+}
